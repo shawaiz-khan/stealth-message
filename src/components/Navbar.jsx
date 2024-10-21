deb7076ba56e30fc0c0d03ef34920dfbee3ee654
Shawaiz Khan <shawaizkhan.biz@gmail.com>
2024-10-21 18:29:38 +0100

    update: Updated the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ export default function Navbar() {
         <header>
             <nav className="bg-white h-20 flex justify-evenly items-center px-5">
                 <h1>Logo</h1> {/* Logo should be here */}
-                <ul className="navigation flex gap-5">
+                <ul className="navigation flex gap-10 font-normal text-sm">
                     <li>PRODUCTS</li>
                     <li>LEARN & SUPPORT</li>
                     <li>COMMUNITY & EVENTS</li>
@@ -22,8 +22,8 @@ export default function Navbar() {
                 </div>
                 <button>Menu</button>
             </nav>
-            <nav className="bg-gray-200 h-10 px-5">
-                Under NavBar
+            <nav className="bg-gray-200 h-10 flex justify-start items-center px-20">
+                Home {'>'} Solutions {'>'} StealthMessage
             </nav>
         </header>
     )
