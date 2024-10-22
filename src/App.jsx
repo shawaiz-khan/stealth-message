6d8d11f232ce1f10458b0b2a2b25e43112ceb1ba
Shawaiz Khan <shawaizkhan.biz@gmail.com>
2024-10-22 17:13:22 +0100

    add: Social icon strip added

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
+import { Facebook, Instagram, Linkedin, Twitter, Youtube } from "lucide-react"
+import Hero from "./components/Hero"
+
 function App() {
   return (
-    <>
-      <h1>Stealth Message</h1>
-    </>
+    <main>
+      <Hero />
+      <div className="social-icons fixed right-4 bottom-4 flex flex-col gap-5 text-blue-800">
+        <Facebook className="hover:text-orange-600" />
+        <Instagram className="hover:text-orange-600" />
+        <Youtube className="hover:text-orange-600" />
+        <Linkedin className="hover:text-orange-600" />
+        <Twitter className="hover:text-orange-600" />
+      </div>
+    </main>
   )
 }
 
---
+++ src/components/Hero.jsx
@@ -0,0 +1,7 @@
+export default function Hero() {
+    return (
+        <div className="h-screen bg-gray-500 p-5">
+            Hero
+        </div>
+    )
+}
